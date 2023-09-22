eea5026636a49906eb0321f72e2b32a78d74ab0d
Diki Center <dikimistak@gmail.com>
2023-09-22 11:17:24 +0700

    tipe data dict

---
+++ dasar_sintaksis_dict.py
@@ -0,0 +1,26 @@
+users = {
+    'id': 1,
+    'name': 'leanne graham',
+    'username': 'bret',
+    'email': 'sincere@april.biz',
+    'address': {
+        'street': 'kulas light',
+        'suite': 'apt. 556',
+        'city': 'gwenborough',
+        'zipcode': '92998-3874',
+        'geo': {
+            'lat': '-37.3159',
+            'lng': '81.1496'
+        }
+    }
+}
+print(users)
+print(users['id'])
+print(users['name'])
+print(users['username'])
+print(users['email'])
+print(users['address'])
+print(users['address']['street'])
+print(users['address']['geo'])
+print(users['address']['geo']['lat'])
+print(users['address']['geo']['lng'])
